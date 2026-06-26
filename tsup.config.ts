a35a97bf1549fab46bd444dadf87aa7066a9d3d9
Anthony COUCKE <183762265+couckedev@users.noreply.github.com>
2026-06-26 16:55:18 +0200

    chore: add subexports entries

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts"],
-	format: ["esm", "cjs"],
-	dts: true,
-	sourcemap: true,
-	clean: true,
-	splitting: false,
-	treeshake: true,
+  entry: [
+    "src/index.ts",
+    "src/adapters/index.ts",
+    "src/errors/index.ts",
+    "src/ports/index.ts",
+    "src/types/index.ts",
+  ],
+  format: ["esm", "cjs"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  splitting: false,
+  treeshake: true,
 });
